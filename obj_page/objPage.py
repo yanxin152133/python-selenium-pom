7e69083cf8150e5c3c0bd1159cd38b1e06567409
yanxin152133 <yanxin152133@gmail.com>
2024-12-20 09:50:25 +0800

    fix(优化):

--- obj_page/objPage.py
+++ obj_page/objPage.py
@@ -8,7 +8,9 @@ class Page(BasePage):
     submit = ('id', 'search_icon')
 
     def test(self, text):
+        self.sleep(2)
         self.get(self.url)
+        self.sleep(2)
         self.input_text(self.search, text)
         self.click(self.submit)
-        self.sleep(10)
+        self.sleep(2)
