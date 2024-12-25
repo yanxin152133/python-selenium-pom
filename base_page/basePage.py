41901d10d60254b040e21ae3258bd0fdb7c8c952
yanxin152133 <yanxin152133@gmail.com>
2024-12-25 09:44:03 +0800

    perf(优化):

--- base_page/basePage.py
+++ base_page/basePage.py
@@ -1,6 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver import Keys
-from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 import time
@@ -9,12 +7,11 @@ import time
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-
-        self.wait = WebDriverWait(driver, 1)
-        self.title()
+        self.wait = WebDriverWait(driver, 30)
 
     def get(self, url):
         self.driver.get(url)
+        self.sleep(2)
 
     def title(self):
         return self.driver.title
@@ -23,14 +20,17 @@ class BasePage:
         return self.driver.find_element(*locator)
 
     def getTitle(self):
+        self.sleep(2)
         return self.driver.find_element(By.XPATH, '/html/head/title').get_attribute("textContent")
 
     def input_text(self, locator, text):
-        self.locator(locator).send_keys(text)
+        self.driver.find_element(*locator).send_keys(text)
+        self.sleep(1)
 
     def click(self, locator):
         # 防止使用click保存进而使用send_keys
         self.driver.find_element(*locator).send_keys(Keys.ENTER)
+        self.sleep(2)
 
     def quit(self):
         self.driver.quit()
--- obj_page/objPage.py
+++ obj_page/objPage.py
@@ -1,16 +1,18 @@
+from selenium.webdriver.support.wait import WebDriverWait
 from base_page.basePage import BasePage
 
 
 class Page(BasePage):
-    url = "https://www.bing.com/?mkt=zh-CN"
+    url = "https://cn.bing.com/"
 
     search = ('id', 'sb_form_q')
     submit = ('id', 'search_icon')
 
     def test(self, text):
-        self.sleep(2)
         self.get(self.url)
-        self.sleep(2)
+        wait = WebDriverWait(self.driver, timeout=10)
+        wait.until(lambda d: self.locator(self.submit).is_displayed())
         self.input_text(self.search, text)
         self.click(self.submit)
         self.sleep(2)
+
